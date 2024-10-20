6dbc1416a49a9003ea33520143916b4b11c14f80
afred <afred@MacBook-Pro.local>
2024-10-21 01:15:29 +0800

    add ip range

--- lib/main.dart
+++ lib/main.dart
@@ -13,28 +13,41 @@ class SpeedTestApp extends StatefulWidget {
   @override
   SpeedTestAppState createState() => SpeedTestAppState();
 }
+
 class SpeedTestAppState extends State<SpeedTestApp> {
   List<String> activeHosts = [];
   Map<String, String> pingResults = {};
   bool isScanning = false;
-  // Define network ranges
   final List<String> networks = [
     "173.245.48.0/20",
-    "103.21.244.0/22",
-    "103.22.200.0/22",
-    "103.31.4.0/22",
-    "141.101.64.0/18",
-    "108.162.192.0/18",
-    "190.93.240.0/20",
-    "188.114.96.0/20",
-    "197.234.240.0/22",
-    "198.41.128.0/17",
-    "162.158.0.0/15",
-    "104.16.0.0/13",
-    "104.24.0.0/14",
-    "172.64.0.0/13",
-    "131.0.72.0/22"
+    // 其他网络范围...
   ];
+
+  Iterable<String> calculateIPRange(String subnet) sync* {
+    RegExp regExp = RegExp(r'(\d+)\.(\d+)\.(\d+)\.(\d+)/(\d+)');
+    Match? match = regExp.firstMatch(subnet);
+
+    if (match != null) {
+      int base1 = int.parse(match.group(1)!);
+      int base2 = int.parse(match.group(2)!);
+      int base3 = int.parse(match.group(3)!);
+      int base4 = int.parse(match.group(4)!);
+      int subnetBits = int.parse(match.group(5)!);
+
+      int ipStart = (base1 << 24) + (base2 << 16) + (base3 << 8) + base4;
+      int numIPs = 1 << (32 - subnetBits);
+      int ipEnd = ipStart + numIPs;
+
+      for (int ip = ipStart; ip < ipEnd; ip++) {
+        int octet1 = (ip >> 24) & 0xFF;
+        int octet2 = (ip >> 16) & 0xFF;
+        int octet3 = (ip >> 8) & 0xFF;
+        int octet4 = ip & 0xFF;
+        yield '$octet1.$octet2.$octet3.$octet4';
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -68,42 +81,46 @@ class SpeedTestAppState extends State<SpeedTestApp> {
       ),
     );
   }
+
   Future<void> scanNetworks() async {
     setState(() {
       isScanning = true;
       activeHosts.clear();
       pingResults.clear();
     });
-    for (var subnet in networks) {
 
-      final stream = NetworkAnalyzer.i.discover2(subnet, 80);
-      // Use the singleton instance of NetworkAnalyzer to discover active hosts
-      await for (final host in stream) {
-        if (host.exists) {
-          setState(() {
-            activeHosts.add(host.ip);
-          });
-          // Ping the discovered host
-          pingHost(host.ip);
-        }
-      }
+    for (var subnet in networks) {
+      await scanSubnet(subnet);
     }
 
     setState(() {
       isScanning = false;
     });
   }
+
+  Future<void> scanSubnet(String subnet) async {
+    final stream = NetworkAnalyzer.i.discover2(subnet, 80, timeout: const Duration(seconds: 2));
+    await for (final host in stream) {
+      if (host.exists) {
+        setState(() {
+          activeHosts.add(host.ip);
+        });
+        // Ping the discovered host
+        await pingHost(host.ip);
+      }
+    }
+  }
+
   Future<void> pingHost(String ip) async {
     final ping = Ping(ip, count: 3);
     String pingTime = "N/A";
-    ping.stream.listen((PingData event) {
-      print(ip+": "+event.toString());
+    await for (final event in ping.stream) {
       if (event.response != null) {
         pingTime = event.response!.time!.inMilliseconds.toString();
         setState(() {
           pingResults[ip] = pingTime;
         });
       }
-    });
+    }
   }
 }
