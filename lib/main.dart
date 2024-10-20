a45e7672a12026f563ed5c3ef525142748d330b1
afred <afred@MacBook-Pro.local>
2024-10-21 00:29:43 +0800

    add ip range

--- lib/main.dart
+++ lib/main.dart
@@ -19,21 +19,21 @@ class SpeedTestAppState extends State<SpeedTestApp> {
   bool isScanning = false;
   // Define network ranges
   final List<String> networks = [
-    "173.245.48.0",
-    "103.21.244.0",
-    "103.22.200.0",
-    "103.31.4.0",
-    "141.101.64.0",
-    "108.162.192.0",
-    "190.93.240.0",
-    "188.114.96.0",
-    "197.234.240.0",
-    "198.41.128.0",
-    "162.158.0.0",
-    "104.16.0.0",
-    "104.24.0.0",
-    "172.64.0.0",
-    "131.0.72.0"
+    "173.245.48.0/20",
+    "103.21.244.0/22",
+    "103.22.200.0/22",
+    "103.31.4.0/22",
+    "141.101.64.0/18",
+    "108.162.192.0/18",
+    "190.93.240.0/20",
+    "188.114.96.0/20",
+    "197.234.240.0/22",
+    "198.41.128.0/17",
+    "162.158.0.0/15",
+    "104.16.0.0/13",
+    "104.24.0.0/14",
+    "172.64.0.0/13",
+    "131.0.72.0/22"
   ];
   @override
   Widget build(BuildContext context) {
@@ -93,12 +93,11 @@ class SpeedTestAppState extends State<SpeedTestApp> {
       isScanning = false;
     });
   }
-
   Future<void> pingHost(String ip) async {
     final ping = Ping(ip, count: 3);
     String pingTime = "N/A";
-
     ping.stream.listen((PingData event) {
+      print(ip+": "+event.toString());
       if (event.response != null) {
         pingTime = event.response!.time!.inMilliseconds.toString();
         setState(() {
